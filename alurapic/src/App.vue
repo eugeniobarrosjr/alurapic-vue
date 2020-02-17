39a3514a4fbe597b73846b49c7ff3d62051beb1d
Eugenio Barros <eugenio.barros.jr@gmail.com>
2020-02-17 11:16:21 -0300

    feature(core) Add Project

---
+++ alurapic/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-bind:key="photo.id" v-for="photo of photos">
+        <img :src="photo.src" :alt="photo.alt" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Alurapic",
+      photos: []
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(
+        photos => (this.photos = photos),
+        error => console.log(error)
+      );
+  }
+};
+</script>
+
+<style></style>
